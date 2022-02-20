58568a0e4aa68b804a8a11bfd01d65bd5954658e
MphoRasenyalo201576673 <ndenderasenyalo@gmail.com>
2022-02-20 22:28:39 +0200

    created the database and a Todo Model

--- todo.py
+++ todo.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("__name__")
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False # to remove the warning after creating the 
+                                                   # database model
+db=SQLAlchemy(app)
+
+
 Bootstrap(app) 
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_Text = db.Column(db.String(100), nullable=False)
+    complete_Status = db.Column(db.Integer, default=0)
+    date_Created=db.Column(db.Date, default = db.DateTime)
+    
+
+    def __repr__(self):
+        return "<Task > %r" % self.id
+
 
 @app.route("/")
 def index():
